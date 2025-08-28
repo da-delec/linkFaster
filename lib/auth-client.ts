303c4b3dba5d38a6f011b5c048b6b73929171d9e
Corentin Delesalle <delesallecorentin3@gmail.com>
2025-08-28 14:25:48 +0200

    bug better auth

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,7 +3,7 @@ import { magicLinkClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
     baseURL: process.env.NODE_ENV === 'production' 
-        ? process.env.NEXT_PUBLIC_APP_URL || (typeof window !== 'undefined' ? window.location.origin : 'https://votre-domain.com')
+        ? process.env.NEXT_PUBLIC_APP_URL || (typeof window !== 'undefined' ? window.location.origin : 'https://linkfaster.link')
         : "http://localhost:3000",
     plugins: [
         magicLinkClient()
